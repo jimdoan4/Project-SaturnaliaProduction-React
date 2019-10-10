9e6ecc6c370961d346ec155b3b6d65c9b416d240
Jim Doan <jimdoan4@gmail.com>
2019-10-10 13:41:05 -0400

    comment out routes

--- routes/index.js
+++ routes/index.js
@@ -6,29 +6,30 @@ const reviewController = require('../controllers/reviewController');
 const eventController = require('../controllers/eventController');
 const galleryController = require('../controllers/galleryController')
 
- router.get('/', appController.index);
- 
+router.get('/', appController.index);
+
+// Event routes
 router.get('/events/', eventController.index);
 router.post('/events/', eventController.create);
 router.get('/events/:eventId/', eventController.show);
 router.put('/events/:eventId/', eventController.update);
 router.delete('/events/:eventId/', eventController.delete);
 
-
+// User routes
 router.get('/users/', userController.index);
 router.post('/users/', userController.create);
 router.get('/users/:userId/', userController.show);
 router.put('/users/:userId/', userController.update);
 router.delete('/users/:userId/', userController.delete);
 
+// Review routes
 router.get('/reviews/', reviewController.index);
 router.post('/reviews/', reviewController.create);
 router.get('/reviews/:reviewId/', reviewController.show);
 router.put('/reviews/:reviewId/', reviewController.update);
 router.delete('/reviews/:reviewId/', reviewController.delete);
 
-
-
+// Gallery routes
 router.get('/gallerys/', galleryController.index);
 router.post('/gallerys/', galleryController.create);
 router.get('/gallerys/:galleryId/', galleryController.show);
